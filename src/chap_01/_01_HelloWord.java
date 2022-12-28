014edf9cb4489fb53136cd6784815ebaab75ff2b
Hwanie91 <jh27hot@gmail.com>
2022-12-28 23:20:35 +0900

    자바 복습

---
+++ src/chap_01/_01_HelloWord.java
@@ -0,0 +1,9 @@
+package chap_01;
+
+public class _01_HelloWord {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+
+}
---
+++ src/chap_01/_Quiz_01.java
@@ -0,0 +1,23 @@
+package chap_01;
+
+public class _Quiz_01 {
+    /*(실행 결과)
+    상암08번 버스
+    약 3분 후 도착
+    남은 거리 1.2km*/
+    public static void main(String[] args) {
+        String bus1 = "1234";
+        String bus2 = "상암08";
+        System.out.println(bus2 + "번 버스");
+
+        int time = 3;
+        int time2 = 5;
+        System.out.println("약 " + time + "분 후 도착");
+
+        double dis = 0.8;
+        double dis2 = 1.2;
+        System.out.println("남은 거리 " + dis2 + "km");
+
+
+    }
+}
