785bfb53c64a0f074ca65c35bcae7e7bed83f931
Hwanie91 <jh27hot@gmail.com>
2023-02-01 20:16:14 +0900

    Update _04_ParameterAndReturn.java

--- src/chap_06/_04_ParameterAndReturn.java
+++ src/chap_06/_04_ParameterAndReturn.java
@@ -6,13 +6,6 @@ public class _04_ParameterAndReturn {
         return result;
     }
 
-//    public static void powerByExp(int number, int exponent) {
-//        int result = 1;
-//        for (int i = 0; i < exponent; i++) {
-//            result *= number;
-//        }
-//        System.out.println(number + " 의 " + exponent + " 승은 " +result);
-//    }
     public static int getPowerByExp(int number, int exponent) {
         int result = 1;
         for (int i = 0; i < exponent; i++) {
